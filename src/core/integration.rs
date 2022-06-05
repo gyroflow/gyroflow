85c5d101c13fab3e2cf9e1aeaf4113755b4b051c
Vladimir Pinchuk <vladimir.pinchuk01@gmail.com>
2022-06-05 22:24:31 +0300

    use timestamps in 'Gyroflow' integrator too; cleanup

--- src/core/integration.rs
+++ src/core/integration.rs
@@ -143,49 +143,26 @@ impl GyroIntegrator for GyroOnlyIntegrator {
     fn integrate(imu_data: &[TimeIMU], duration_ms: f64) -> TimeQuat {
         if imu_data.is_empty() { return BTreeMap::new(); }
         let mut quats = BTreeMap::new();
-        // let gyro_sample_rate = imu_data.len() as f64 / (duration_ms / 1000.0);
-        let sample_time_s = duration_ms / 1000.0 / imu_data.len() as f64;
         let mut orientation = UnitQuaternion::from_euler_angles(std::f64::consts::FRAC_PI_2, 0.0, 0.0);
-
-        // let start_time_s = imu_data[0].timestamp / 1000.0;
-
-        // let mut i: i32 = 0;
+                
+        let sample_time_ms = duration_ms / 1000.0 / imu_data.len() as f64;
+        let mut prev_time = imu_data[0].timestamp_ms - sample_time_ms;
 
         for v in imu_data {
             if let Some(g) = v.gyro.as_ref() {
                 let omega = Vector3::new(-g[1], g[0], g[2]) * (std::f64::consts::PI / 180.0);
-                // let accl  = Vector3::new(-v.accl[1], v.accl[0], v.accl[2]).normalize();
-
-                // let last_time = imu_data[(i-1).max(0) as usize].timestamp / 1000.0;
-                // let this_time = imu_data[i as usize].timestamp / 1000.0;
-                // let next_time = imu_data[(i+1).min(imu_data.len() as i32-1) as usize].timestamp / 1000.0;
-
-                // let delta_time = (next_time - last_time) / 2.0;
-
-                fn rate_to_quat(omega: Vector3<f64>, dt: f64) -> Quaternion<f64> {
-                    // https://stackoverflow.com/questions/24197182/efficient-quaternion-angular-velocity/24201879#24201879
-                    // no idea how it fully works, but it does
-                    let mut ha = omega * dt * 0.5;
-                    let l = ha.dot(&ha).sqrt();
-
-                    if l > 1.0e-12 {
-                        ha *= l.sin() / l;
-                        Quaternion::from_parts(l.cos(), ha).normalize()
-                    } else {
-                        Quaternion::from_parts(1.0, Vector3::from_element(0.0))
-                    }
-                }
 
                 // calculate rotation quaternion
-                let delta_q = rate_to_quat(omega, sample_time_s);
+                let dt = (v.timestamp_ms - prev_time) / 1000.0;
+                let delta_q = UnitQuaternion::from_scaled_axis(omega * dt);
 
                 // rotate orientation by this quaternion
-                orientation = Quat64::from_quaternion(orientation.quaternion() * delta_q);
+                orientation = Quat64::from_quaternion(orientation.quaternion() * delta_q.quaternion());
 
                 quats.insert((v.timestamp_ms * 1000.0) as i64, orientation);
 
+                prev_time = v.timestamp_ms;
             }
-            // i += 1;
         }
 
         quats
@@ -202,7 +179,7 @@ impl GyroIntegrator for ComplementaryIntegrator {
     fn integrate(imu_data: &[TimeIMU], duration_ms: f64) -> TimeQuat {
         if imu_data.is_empty() { return BTreeMap::new(); }
         let mut quats = BTreeMap::new();
-        let sample_time_s = duration_ms / 1000.0 / imu_data.len() as f64;
+        let sample_time_ms = duration_ms / imu_data.len() as f64;
         let init_pos = UnitQuaternion::from_euler_angles(0.0, std::f64::consts::FRAC_PI_2, 0.0);
 
         let mut f = ComplementaryFilter::default();
@@ -210,7 +187,7 @@ impl GyroIntegrator for ComplementaryIntegrator {
         f.set_orientation(init_pos_q.scalar(), init_pos_q.vector()[0], init_pos_q.vector()[1], init_pos_q.vector()[2]);
 
         const DEG2RAD: f64 = std::f64::consts::PI / 180.0;
-        let mut prev_time = imu_data[0].timestamp_ms - sample_time_s;
+        let mut prev_time = imu_data[0].timestamp_ms - sample_time_ms;
         for v in imu_data {
             if let Some(g) = v.gyro.as_ref() {
                 let mut a = v.accl.unwrap_or_default();
