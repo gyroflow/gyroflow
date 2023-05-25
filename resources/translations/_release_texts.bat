dcd5f34f473a47d8053e9b63848c4769b0e7f31a
AdrianEddy <adrian.eddy@gmail.com>
2023-05-25 05:43:41 +0200

    Update Qt to 6.5.1

--- resources/translations/_release_texts.bat
+++ resources/translations/_release_texts.bat
@@ -1,1 +1,1 @@
-forfiles /s /m *.ts /c "cmd /c ..\..\ext\6.5.0\msvc2019_64\bin\lrelease.exe @path"
+forfiles /s /m *.ts /c "cmd /c ..\..\ext\6.5.1\msvc2019_64\bin\lrelease.exe @path"
--- resources/translations/_update_texts.bat
+++ resources/translations/_update_texts.bat
@@ -1,1 +1,1 @@
-forfiles /s /m *.ts /c "cmd /c ..\..\ext\6.5.0\msvc2022_64\bin\lupdate.exe ../../src/ui/ -ts @path"
+forfiles /s /m *.ts /c "cmd /c ..\..\ext\6.5.1\msvc2022_64\bin\lupdate.exe ../../src/ui/ -ts @path"
