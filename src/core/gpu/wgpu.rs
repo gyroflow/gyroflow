938b36a29dd99d341bf120f43971136419f68228
AdrianEddy <adrian.eddy@gmail.com>
2023-09-19 21:38:09 +0200

    Fix compilation

--- src/core/gpu/wgpu.rs
+++ src/core/gpu/wgpu.rs
@@ -424,7 +424,7 @@ impl WgpuWrapper {
                         resolve_target: None,
                         ops: wgpu::Operations {
                             load: wgpu::LoadOp::Clear(wgpu::Color::BLACK),
-                            store: true,
+                            store: wgpu::StoreOp::Store,
                         },
                     })],
                     depth_stencil_attachment: None,
