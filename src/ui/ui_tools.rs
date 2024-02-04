b180e8b3d6553ca7f81c2f91d43ba307bfc3ced1
Korney Czukowski <carbofos@seznam.cz>
2024-02-04 13:13:09 +0100

    Increment values in number field using up/down keys based on cursor position

--- src/ui/ui_tools.rs
+++ src/ui/ui_tools.rs
@@ -27,6 +27,7 @@ pub struct UITools {
     set_icon: qt_method!(fn(&mut self, wnd: QJSValue)),
     get_safe_area_margins: qt_method!(fn(&mut self, wnd: QJSValue) -> QJsonObject),
     set_progress: qt_method!(fn(&self, progress: f64)),
+    modify_digit: qt_method!(fn(&self, value: String, cursor_position: usize, increase: bool) -> QString),
     closing: qt_method!(fn(&mut self)),
 
     language_changed: qt_signal!(),
@@ -218,5 +219,197 @@ impl UITools {
                 calib_ctl.borrow_mut().stabilizer.params.write().framebuffer_inverted = util::is_opengl();
             }
         //}
+    }
+
+    pub fn modify_digit(&self, value: String, cursor_position: usize, increase: bool) -> QString {
+        let (new_num_str, new_cursor_position) = modify_digit_impl(value.as_str(), cursor_position, increase);
+
+        QString::from(format!("{};{}", new_num_str, new_cursor_position))
+    }
+}
+
+
+pub fn modify_digit_impl(num_str: &str, cursor_position: usize, increase: bool) -> (String, usize) {
+    // Convert the string to a number and find its decimal point
+    let number = num_str.parse::<f64>().unwrap();
+    let number_length = num_str.len();
+    let decimal_pos = num_str.find('.').unwrap_or(number_length);
+    let has_decimal_part = number_length > decimal_pos;
+    let decimal_length = if has_decimal_part { number_length - decimal_pos - 1 } else { 0 };
+
+    assert!(cursor_position <= number_length, "Cursor position out of bounds");
+
+    // Calculate the position offset from the decimal point
+    let mut pos_adjust: isize = 0;
+    if cursor_position > decimal_pos {
+        pos_adjust -= 1;
+    } else if has_decimal_part && cursor_position == decimal_pos {
+        pos_adjust -= 1;
+    }
+    if cursor_position == number_length {
+        pos_adjust -= 1;
+    } else if cursor_position == 0 && number < 0.0 {
+        pos_adjust += 1;
+    }
+
+    // Calculate the value to add or subtract
+    let dec_offset: isize = (-1 * pos_adjust)
+        .checked_add_unsigned(decimal_pos).unwrap()
+        .checked_sub_unsigned(cursor_position + 1).unwrap();
+    let modifier = 10f64.powi(dec_offset as i32);
+
+    // Modify the number
+    let new_number = if increase {
+        if number < 0.0 && modifier + number > 0.0 { -1.0 * number } else { number + modifier }
+    } else {
+        if number > 0.0 && modifier - number > 0.0 { -1.0 * number } else { number - modifier }
+    };
+    let new_num_negative = new_number < 0.0;
+
+    // Calculate padding length
+    let new_num_str_abs = format!("{:1.1$}", new_number.abs(), decimal_length);
+    let new_num_length = new_num_str_abs.len() + if new_num_negative { 1 } else { 0 };
+    let new_num_length_diff: isize = if (number < 0.0) ^ (new_number < 0.0) {
+        if number < 0.0 { 1 } else { -1 }
+    } else {
+        0
+    };
+    let new_num_pad_length = number_length.checked_sub(new_num_length.checked_add_signed(new_num_length_diff).unwrap()).unwrap_or(0);
+
+    // Adjust the cursor position if the number of digits has changed
+    let new_num_str = format!(
+        "{}{}{}",
+        if new_num_negative { "-" } else { "" },
+        "0".repeat(new_num_pad_length),
+        new_num_str_abs,
+    );
+    let new_cursor_position = (new_num_length + new_num_pad_length).saturating_sub(number_length - cursor_position);
+
+    (new_num_str, new_cursor_position)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_case::test_case;
+
+    #[test_case(("0", 0, true), ("1", 0))]
+    #[test_case(("0", 0, false), ("-1", 1))]
+    #[test_case(("0", 1, true), ("1", 1))]
+    #[test_case(("0", 1, false), ("-1", 2))]
+    #[test_case(("00", 0, true), ("10", 0))]
+    #[test_case(("00", 0, false), ("-10", 1))]
+    #[test_case(("00", 1, true), ("01", 1))]
+    #[test_case(("00", 1, false), ("-01", 2))]
+    #[test_case(("1", 0, true), ("2", 0))]
+    #[test_case(("1", 0, false), ("0", 0))]
+    #[test_case(("1", 1, true), ("2", 1))]
+    #[test_case(("1", 1, false), ("0", 1))]
+    #[test_case(("-1", 0, true), ("0", 0))]
+    #[test_case(("-1", 0, false), ("-2", 0))]
+    #[test_case(("-1", 1, true), ("0", 0))]
+    #[test_case(("-1", 1, false), ("-2", 1))]
+    #[test_case(("-1", 2, true), ("0", 1))]
+    #[test_case(("-1", 2, false), ("-2", 2))]
+    #[test_case(("9", 0, true), ("10", 1))]
+    #[test_case(("9", 0, false), ("8", 0))]
+    #[test_case(("-9", 1, true), ("-8", 1))]
+    #[test_case(("-9", 1, false), ("-10", 2))]
+    #[test_case(("10", 0, true), ("20", 0))]
+    #[test_case(("10", 0, false), ("00", 0))]
+    #[test_case(("10", 1, true), ("11", 1))]
+    #[test_case(("10", 1, false), ("09", 1))]
+    #[test_case(("10", 2, true), ("11", 2))]
+    #[test_case(("10", 2, false), ("09", 2))]
+    #[test_case(("-10", 0, true), ("00", 0))]
+    #[test_case(("-10", 0, false), ("-20", 0))]
+    #[test_case(("-10", 1, true), ("00", 0))]
+    #[test_case(("-10", 1, false), ("-20", 1))]
+    #[test_case(("-10", 2, true), ("-09", 2))]
+    #[test_case(("-10", 2, false), ("-11", 2))]
+    #[test_case(("-10", 3, true), ("-09", 3))]
+    #[test_case(("-10", 3, false), ("-11", 3))]
+    #[test_case(("11", 0, true), ("21", 0))]
+    #[test_case(("11", 0, false), ("01", 0))]
+    #[test_case(("11", 1, true), ("12", 1))]
+    #[test_case(("11", 1, false), ("10", 1))]
+    #[test_case(("-11", 1, true), ("-01", 1))]
+    #[test_case(("-11", 1, false), ("-21", 1))]
+    #[test_case(("-11", 2, true), ("-10", 2))]
+    #[test_case(("-11", 2, false), ("-12", 2))]
+    #[test_case(("19", 0, true), ("29", 0))]
+    #[test_case(("19", 0, false), ("09", 0))]
+    #[test_case(("19", 1, true), ("20", 1))]
+    #[test_case(("19", 1, false), ("18", 1))]
+    #[test_case(("-19", 1, true), ("-09", 1))]
+    #[test_case(("-19", 1, false), ("-29", 1))]
+    #[test_case(("-19", 2, true), ("-18", 2))]
+    #[test_case(("-19", 2, false), ("-20", 2))]
+    #[test_case(("999", 2, true), ("1000", 3))]
+    #[test_case(("999", 2, false), ("998", 2))]
+    #[test_case(("-999", 3, true), ("-998", 3))]
+    #[test_case(("-999", 3, false), ("-1000", 4))]
+    #[test_case(("0.0", 0, true), ("1.0", 0))]
+    #[test_case(("0.0", 0, false), ("-1.0", 1))]
+    #[test_case(("0.0", 1, true), ("1.0", 1))]
+    #[test_case(("0.0", 1, false), ("-1.0", 2))]
+    #[test_case(("0.0", 2, true), ("0.1", 2))]
+    #[test_case(("0.0", 2, false), ("-0.1", 3))]
+    #[test_case(("0.0", 3, true), ("0.1", 3))]
+    #[test_case(("0.0", 3, false), ("-0.1", 4))]
+    #[test_case(("0.00", 2, true), ("0.10", 2))]
+    #[test_case(("0.00", 2, false), ("-0.10", 3))]
+    #[test_case(("0.00", 3, true), ("0.01", 3))]
+    #[test_case(("0.00", 3, false), ("-0.01", 4))]
+    #[test_case(("-0.2", 0, true), ("0.2", 0))]
+    #[test_case(("-0.2", 0, false), ("-1.2", 0))]
+    #[test_case(("0.2", 0, true), ("1.2", 0))]
+    #[test_case(("0.2", 0, false), ("-0.2", 1))]
+    #[test_case(("-0.5", 1, true), ("0.5", 0))]
+    #[test_case(("0.5", 0, false), ("-0.5", 1))]
+    #[test_case(("-00.5", 2, true), ("00.5", 1))]
+    #[test_case(("00.5", 1, false), ("-00.5", 2))]
+    #[test_case(("1.0", 0, true), ("2.0", 0))]
+    #[test_case(("1.0", 0, false), ("0.0", 0))]
+    #[test_case(("1.0", 2, true), ("1.1", 2))]
+    #[test_case(("1.0", 2, false), ("0.9", 2))]
+    #[test_case(("-1.0", 1, true), ("0.0", 0))]
+    #[test_case(("-1.0", 1, false), ("-2.0", 1))]
+    #[test_case(("-1.0", 3, true), ("-0.9", 3))]
+    #[test_case(("-1.0", 3, false), ("-1.1", 3))]
+    #[test_case(("12.345", 0, true), ("22.345", 0))]
+    #[test_case(("12.345", 1, true), ("13.345", 1))]
+    #[test_case(("12.345", 2, true), ("13.345", 2))]
+    #[test_case(("12.345", 3, true), ("12.445", 3))]
+    #[test_case(("12.345", 4, true), ("12.355", 4))]
+    #[test_case(("12.345", 5, true), ("12.346", 5))]
+    #[test_case(("12.345", 6, true), ("12.346", 6))]
+    #[test_case(("12.345", 0, false), ("02.345", 0))]
+    #[test_case(("12.345", 1, false), ("11.345", 1))]
+    #[test_case(("12.345", 2, false), ("11.345", 2))]
+    #[test_case(("12.345", 3, false), ("12.245", 3))]
+    #[test_case(("12.345", 4, false), ("12.335", 4))]
+    #[test_case(("12.345", 5, false), ("12.344", 5))]
+    #[test_case(("12.345", 6, false), ("12.344", 6))]
+    fn test_modify_digit_impl(input: (&str, usize, bool), expected: (&str, usize)) {
+        let actual = modify_digit_impl(input.0, input.1, input.2);
+
+        assert_eq!(
+            expected,
+            (actual.0.as_str(), actual.1),
+            "test_case((\"{}\", {}, {}), (\"{}\", {}))\n input: {}{}  key: [{}]\nexpect: \"{}|{}\"\nactual: \"{}|{}\"",
+            input.0,
+            input.1,
+            input.2,
+            expected.0,
+            expected.1,
+            if expected.0.len() > input.0.len() { " " } else { "" },
+            format!("\"{}{}{}\"", input.0.get(0..input.1).unwrap_or(""), "|", input.0.get(input.1..).unwrap_or("")),
+            if input.2 { "↑" } else { "↓" },
+            expected.0.get(0..expected.1).unwrap_or(""),
+            expected.0.get(expected.1..).unwrap_or(""),
+            actual.0.get(0..actual.1).unwrap_or(""),
+            actual.0.get(actual.1..).unwrap_or(""),
+        );
     }
 }
