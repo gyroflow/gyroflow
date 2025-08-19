6d4a12aff8d327eae1d751260fbaeec7e39b36a3
AdrianEddy <adrian.eddy@gmail.com>
2025-08-19 04:48:20 +0200

    Update required driver version check

--- src/external_sdk/legacy_nvenc.rs
+++ src/external_sdk/legacy_nvenc.rs
@@ -7,12 +7,16 @@ impl LegacyNvenc {
     pub fn is_installed() -> bool {
         use nvml_wrapper::Nvml;
         if let Ok(nvml) = Nvml::init() {
-            if let Ok(ver) = nvml.sys_driver_version() {
+            if let Ok(mut ver) = nvml.sys_driver_version() {
                 log::info!("NVIDIA driver version: {ver}");
-                if let Some((major, _minor)) = ver.split_once('.') {
-                    if let Ok(major) = major.parse::<u32>() {
-                        // If driver is older than 522, we need legacy avcodec, because NVENC12 breaks ABI
-                        if major < 522 {
+                if ver.split('.').count() < 3 {
+                    ver.push_str(".0");
+                }
+                let min_version = if cfg!(target_os = "windows") { "551.76.0" } else { "550.54.14" };
+                if let Ok(min_version) = semver::Version::parse(min_version) {
+                    if let Ok(this_version) = semver::Version::parse(&ver) {
+                        if this_version < min_version {
+                            log::error!("NVIDIA driver version {ver} is too old, minimum required is {min_version}");
                             return false;
                         }
                     }
